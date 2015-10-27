10fa74c1fdd0b04293fc6d0c519d6ee3cddec92a
Aurélien Bompard <aurelien@bompard.org>
2015-10-27 14:13:48 +0100

    Avoid a Pylint warning

--- hyperkitty/tests/views/test_accounts.py
+++ hyperkitty/tests/views/test_accounts.py
@@ -90,11 +90,11 @@ class AccountViewsTestCase(TestCase):
     def test_public_profile_as_oneself(self):
         user_id = uuid.uuid1()
         self.mailman_client.get_list.side_effect = lambda name: FakeMMList(name)
-        self.mm_user = Mock()
-        self.mailman_client.get_user.side_effect = lambda name: self.mm_user
-        self.mm_user.user_id = user_id.int
-        self.mm_user.created_on = None
-        self.mm_user.addresses = ["test@example.com"]
+        mm_user = Mock()
+        self.mailman_client.get_user.side_effect = lambda name: mm_user
+        mm_user.user_id = user_id.int
+        mm_user.created_on = None
+        mm_user.addresses = ["test@example.com"]
         self.client.login(username='testuser', password='testPass')
         response = self.client.get(reverse("hk_public_user_profile",
                                    args=[user_id.int]))
