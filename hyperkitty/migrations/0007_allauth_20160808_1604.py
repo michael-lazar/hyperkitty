51fd203e0a02344200698ac3ac833b8827c0b3dc
Aurélien Bompard <aurelien@bompard.org>
2016-08-09 10:16:42 +0200

    Fix migration to work on empty databases too

--- hyperkitty/migrations/0007_allauth_20160808_1604.py
+++ hyperkitty/migrations/0007_allauth_20160808_1604.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function
 
-from django.db import migrations, models, IntegrityError
+from django.db import migrations, models, IntegrityError, connection, utils
 
 
 PROVIDERS_MAP = {
@@ -26,42 +26,37 @@ def populate_emailaddress(apps, schema_editor):
                   % (user.username, e))
 
 
-#def migrate_social_users(apps, schema_editor):
-#    # Migrate the Social Auth association to AllAuth
-#    UserSocialAuth = apps.get_model("social_auth", "UserSocialAuth")
-#    SocialAccount = apps.get_model("socialaccount", "SocialAccount")
-#    for user_socialauth in UserSocialAuth.objects.all():
-#        if user_socialauth.provider not in PROVIDERS_MAP:
-#            continue  # unsupported provider
-#        try:
-#            SocialAccount.objects.create(
-#                    provider=PROVIDERS_MAP[user_socialauth.provider],
-#                    uid=user_socialauth.uid,
-#                    user=user_socialauth.user,
-#                    last_login=user_socialauth.user.last_login,
-#                    date_joined=user_socialauth.user.date_joined,
-#                    extra_data='{}',
-#                )
-#        except IntegrityError as e:
-#            print("Can't create SocialAccount for %s: %s"
-#                  % (user_socialauth.uid, e))
-
-
-def get_social_users_migration_sql():
+def migrate_social_users(apps, schema_editor):
     # Migrate the Social Auth association to AllAuth
-    # We can't use Python because the social_auth app has been removed, and
-    # thus the UserSocialAuth model isn't available.
-    sql = """
-        INSERT INTO socialaccount_socialaccount
-            (provider, uid, user_id, last_login, date_joined, extra_data)
-        SELECT
-            '%(prov_to)s', uid, user_id, last_login, date_joined, '{}'
-        FROM social_auth_usersocialauth usa
-        JOIN auth_user ON usa.user_id = auth_user.id
-        WHERE provider = '%(prov_from)s';
-        """
-    return [sql % {"prov_from": key, "prov_to": value}
-            for key, value in PROVIDERS_MAP.items()]
+    SocialAccount = apps.get_model("socialaccount", "SocialAccount")
+    # We can't use the UserSocialAuth model because the social_auth app has
+    # been removed, and thus the model isn't available.
+    cursor = connection.cursor()
+    try:
+        cursor.execute("SELECT 1 from social_auth_usersocialauth")
+    except utils.OperationalError:
+        # No social_auth table, stop here.
+        return
+    for provider_old, provider_new in PROVIDERS_MAP.items():
+        cursor.execute("""
+            SELECT uid, user_id, last_login, date_joined
+            FROM social_auth_usersocialauth usa
+            JOIN auth_user ON usa.user_id = auth_user.id
+            WHERE provider = %s
+            """, (provider_old,))
+        for row in cursor:
+            try:
+                SocialAccount.objects.create(
+                        provider=provider_new,
+                        uid=row[0],
+                        user_id=row[1],
+                        last_login=row[2],
+                        date_joined=row[3],
+                        extra_data={},
+                    )
+            except IntegrityError as e:
+                print("Can't create SocialAccount for %s: %s"
+                      % (row.uid, e))
 
 
 class Migration(migrations.Migration):
@@ -74,6 +69,5 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunPython(populate_emailaddress),
-        #migrations.RunPython(migrate_social_users),
-        migrations.RunSQL(get_social_users_migration_sql()),
+        migrations.RunPython(migrate_social_users),
     ]
