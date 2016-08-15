a5aae3592e9a238b9d86e804cc7b5495e948a03a
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2016-08-16 00:50:37 +0530

    Toggle message font on click

--- hyperkitty/static/hyperkitty/js/hyperkitty-thread.js
+++ hyperkitty/static/hyperkitty/js/hyperkitty-thread.js
@@ -370,3 +370,13 @@ function setup_reattach() {
         });
     }).submit();
 }
+
+
+/*
+ * Handle click events
+ */
+function handle_font_click() {
+    $(document).on("click", ".toggle-font", function() {
+        $(this).parents('.email').find('.email-body').toggleClass('fixed')
+    });
+}
