michael-lazar/hyperkitty

commit a55d6493bb93554d44d21c28be9863072805778d

Improve the overview page loading time

hyperkitty/static/hyperkitty/js/hyperkitty-overview.js
@@ -76,4 +76,9 @@ function setup_overview(recent_activity_url) {
             $(this).remove();
         }
     });
+
+    // Page fragments
+    $('div[data-load-from]').each(function() {
+        $(this).load($(this).attr("data-load-from"));
+    });
 }

hyperkitty/models/mailinglist.py
@@ -169,17 +169,20 @@ class MailingList(models.Model):
         cache.delete("MailingList:%s:recent_participants_count" % self.name)
         cache.delete("MailingList:%s:p_count_for:%s:%s"
                      % (self.name, email.date.year, email.date.month))
+        cache.delete("MailingList:%s:top_threads" % self.name)
         # don't warm up the cache in batch mode (mass import)
         if not getattr(settings, "HYPERKITTY_BATCH_MODE", False):
             # TODO: async task
             self.recent_participants_count
             self.get_participants_count_for_month(
                 email.date.year, email.date.month)
+            self.top_threads
 
     on_email_deleted = on_email_added
 
     def on_vote_added(self, vote):
-        pass
+        cache.delete("MailingList:%s:popular_threads" % self.name)
+        self.popular_threads  # TODO: async task
 
     on_vote_deleted = on_vote_added
 
@@ -224,6 +227,49 @@ class MailingList(models.Model):
         # It's not actually necessary to convert back to instances since it's
         # only used in templates where access to instance attributes or
         # dictionnary keys is identical
+
+    @property
+    def top_threads(self):
+        """Threads with the most answers."""
+        cache_key = "MailingList:%s:top_threads" % self.name
+        thread_ids = cache.get(cache_key)
+        if thread_ids is None:
+            # Filter on the recent_threads ids instead of re-using the date
+            # filter, otherwise the Sum will be computed for every thread
+            # regardless of their date.
+            threads = Thread.objects.filter(
+                id__in=[t.id for t in self.recent_threads]).annotate(
+                models.Count("emails")).order_by("-emails__count")[:20]
+            cache.set(cache_key, [t.id for t in threads], 3600 * 12)  # 12h
+        else:
+            threads = Thread.objects.filter(id__in=thread_ids)
+        return threads
+
+    @property
+    def popular_threads(self):
+        """Threads with the most votes."""
+        cache_key = "MailingList:%s:popular_threads" % self.name
+        thread_ids = cache.get(cache_key)
+        if thread_ids is None:
+            # Filter on the recent_threads ids instead of re-using the date
+            # filter, otherwise the Sum will be computed for every thread
+            # regardless of their date.
+            threads = Thread.objects.filter(
+                id__in=[t.id for t in self.recent_threads]).annotate(
+                models.Sum("emails__votes__value")).order_by(
+                "-emails__votes__value__sum")[:20]
+            for thread in threads:
+                value = thread.emails__votes__value__sum
+                if value is None:
+                    value = 0
+                cache.set("Thread:%s:votes_total" % thread.id, value, None)
+            cache.set(
+                cache_key,
+                [t.id for t in threads if t.votes_total > 0],
+                3600 * 12)  # 12h
+        else:
+            threads = Thread.objects.filter(id__in=thread_ids)
+        return threads
 
     def update_from_mailman(self):
         try:

hyperkitty/models/thread.py
@@ -196,6 +196,7 @@ class Thread(models.Model):
 
     def on_vote_added(self, vote):
         cache.delete("Thread:%s:votes" % self.id)
+        cache.delete("Thread:%s:votes_total" % self.id)
         self.get_votes()  # TODO: async task
 
     on_vote_deleted = on_vote_added

hyperkitty/templates/hyperkitty/fragments/overview_top_posters.html
@@ -1,0 +1,31 @@
+{% load i18n %}
+{% load hk_generic %}
+{% load gravatar %}
+
+{% for poster in mlist.top_posters %}
+<div class="maker row">
+    <div class="inline-block maker-id col-tn-1">
+        #{{forloop.counter}}
+    </div>
+    <div class="inline-block gravatar col-tn-3 col-xs-4 col-md-3">
+        {% gravatar poster.address 120 %}
+        <br />
+    </div>
+    <div class="gravatar-details inline-block col-tn-7 col-xs-6 col-md-7">
+        <ul class="list-unstyled">
+            <li><span class="maker-name">
+                {% if poster.mailman_id %}
+                <a href="{% url 'hk_public_user_profile' user_id=poster.mailman_id %}"
+                   title="{% trans "See the profile" %}"
+                    >{{ poster.name|escapeemail }}</a>
+                {% else %}
+                    {{ poster.name|escapeemail }}
+                {% endif %}
+            </span></li>
+            <li><span class="score">{{ poster.count }}</span> {% trans "posts" %}</li>
+        </ul>
+    </div>
+</div>
+{% empty %}
+<p>{% trans "No posters this month (yet)." %}</p>
+{% endfor %}

hyperkitty/templates/hyperkitty/overview.html
@@ -94,13 +94,17 @@
             <h3>{% trans "Discussions You've Flagged" %} ({{ flagged_threads|length }})
                 {% if flagged_threads %}<i class="fa fa-caret-right"></i>{% endif %}
             </h3>
-            {% include "hyperkitty/fragments/overview_threads.html" with threads=flagged_threads empty=empty_messages.flagged %}
+            <div data-load-from="{% url 'hk_list_overview_favorites' mlist.name %}">
+                <img alt="{% trans 'Loading...' %}" class="ajaxloader" src="{% static 'hyperkitty/img/ajax-loader.gif' %}" />
+            </div>
         </section>
         <section id="posted-to">
             <h3>{% trans "Discussions You've Posted to" %} ({{ threads_posted_to|length }})
                 {% if threads_posted_to %}<i class="fa fa-caret-right"></i>{% endif %}
             </h3>
-            {% include "hyperkitty/fragments/overview_threads.html" with threads=threads_posted_to empty=empty_messages.posted %}
+            <div data-load-from="{% url 'hk_list_overview_posted_to' mlist.name %}">
+                <img alt="{% trans 'Loading...' %}" class="ajaxloader" src="{% static 'hyperkitty/img/ajax-loader.gif' %}" />
+            </div>
         </section>
     </div> <!-- /home, personalized threads lists -->
     {% endif %}
@@ -110,19 +114,25 @@
         <div class="anchor-link"><a id="most-recent"></a></div>
         <section id="most-recent">
             <h3>{% trans "Recently active discussions" %}</h3>
-            {% include "hyperkitty/fragments/overview_threads.html" with threads=most_active_threads empty=empty_messages.active %}
+            <div data-load-from="{% url 'hk_list_overview_recent_threads' mlist.name %}">
+                <img alt="{% trans 'Loading...' %}" class="ajaxloader" src="{% static 'hyperkitty/img/ajax-loader.gif' %}" />
+            </div>
         </section>
 
         <div class="anchor-link"><a id="most-popular"></a></div>
         <section id="most-popular">
             <h3>{% trans "Most popular discussions" %}</h3>
-            {% include "hyperkitty/fragments/overview_threads.html" with threads=pop_threads empty=empty_messages.popular %}
+            <div data-load-from="{% url 'hk_list_overview_pop_threads' mlist.name %}">
+                <img alt="{% trans 'Loading...' %}" class="ajaxloader" src="{% static 'hyperkitty/img/ajax-loader.gif' %}" />
+            </div>
         </section>
 
         <div class="anchor-link"><a id="most-active"></a></div>
         <section id="most-active">
             <h3>{% trans "Most active discussions" %}</h3>
-            {% include "hyperkitty/fragments/overview_threads.html" with threads=top_threads empty=empty_messages.active %}
+            <div data-load-from="{% url 'hk_list_overview_top_threads' mlist.name %}">
+                <img alt="{% trans 'Loading...' %}" class="ajaxloader" src="{% static 'hyperkitty/img/ajax-loader.gif' %}" />
+            </div>
         </section>
 
         {% if use_mockups %}
@@ -165,31 +175,9 @@
         <div class="row">
         <section id="most-active-poster" class="col-tn-12 col-xs-6 col-sm-12">
             <h3>{% trans "Most active posters" %}</h3>
-            {% for poster in mlist.top_posters %}
-            <div class="maker row">
-                <div class="inline-block maker-id col-tn-1">
-                    #{{forloop.counter}}
-                </div>
-                <div class="inline-block gravatar col-tn-3 col-xs-4 col-md-3">
-                    {% gravatar poster.address 120 %}
-                    <br />
-                </div>
-                <div class="gravatar-details inline-block col-tn-7 col-xs-6 col-md-7">
-                    <ul class="list-unstyled">
-                        <li><span class="maker-name">
-                            {% if poster.mailman_id %}
-                            <a href="{% url 'hk_public_user_profile' user_id=poster.mailman_id %}"
-                               title="{% trans "See the profile" %}"
-                                >{{ poster.name|escapeemail }}</a>
-                            {% else %}
-                                {{ poster.name|escapeemail }}
-                            {% endif %}
-                        </span></li>
-                        <li><span class="score">{{ poster.count }}</span> {% trans "posts" %}</li>
-                    </ul>
-                </div>
+            <div data-load-from="{% url 'hk_list_overview_top_posters' mlist.name %}">
+                <img alt="{% trans 'Loading...' %}" class="ajaxloader" src="{% static 'hyperkitty/img/ajax-loader.gif' %}" />
             </div>
-            {% endfor %}
 
         </section>
 

hyperkitty/tests/views/test_accounts.py
@@ -280,12 +280,17 @@ class LastViewsTestCase(TestCase):
         self.assertContains(response, "fa-envelope",
                             count=2, status_code=200)
 
-    def test_overview(self):
-        response = self.client.get(
-            reverse('hk_list_overview', args=["list@example.com"]))
-        # 2 in recently active, 2 in most active
+    def test_overview_top_threads(self):
+        response = self.client.get(reverse(
+            'hk_list_overview_top_threads', args=["list@example.com"]))
         self.assertContains(response, "fa-envelope",
-                            count=4, status_code=200)
+                            count=2, status_code=200)
+
+    def test_overview_recent_threads(self):
+        response = self.client.get(reverse(
+            'hk_list_overview_recent_threads', args=["list@example.com"]))
+        self.assertContains(response, "fa-envelope",
+                            count=2, status_code=200)
 
 
 class SubscriptionsTestCase(TestCase):

hyperkitty/tests/views/test_archives.py
@@ -80,9 +80,24 @@ class ListArchivesTestCase(TestCase):
             'hk_list_overview', args=["list@example.com"]))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context["view_name"], "overview")
-        self.assertEqual(len(response.context["top_threads"]), 1)
-        self.assertEqual(len(response.context["most_active_threads"]), 1)
-        self.assertEqual(len(response.context["pop_threads"]), 0)
+
+    def test_overview_top_threads(self):
+        response = self.client.get(reverse(
+            'hk_list_overview_top_threads', args=["list@example.com"]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context["threads"]), 1)
+
+    def test_overview_recent_threads(self):
+        response = self.client.get(reverse(
+            'hk_list_overview_recent_threads', args=["list@example.com"]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context["threads"]), 1)
+
+    def test_overview_pop_threads(self):
+        response = self.client.get(reverse(
+            'hk_list_overview_pop_threads', args=["list@example.com"]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context["threads"]), 0)
 
     def test_overview_with_user(self):
         user = User.objects.create_user(
@@ -99,8 +114,15 @@ class ListArchivesTestCase(TestCase):
         response = self.client.get(
             reverse('hk_list_overview', args=["list@example.com"]))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.context["threads_posted_to"]), 1)
-        self.assertEqual(len(response.context["flagged_threads"]), 1)
+        self.assertEqual(response.context["view_name"], "overview")
+        response = self.client.get(
+            reverse('hk_list_overview_posted_to', args=["list@example.com"]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context["threads"]), 1)
+        response = self.client.get(
+            reverse('hk_list_overview_favorites', args=["list@example.com"]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context["threads"]), 1)
 
     def test_overview_cleaned_cache(self):
         # Test the overview page with a clean cache (different code path for
@@ -109,14 +131,23 @@ class ListArchivesTestCase(TestCase):
         response = self.client.get(
             reverse('hk_list_overview', args=["list@example.com"]))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context["view_name"], "overview")
-        self.assertEqual(len(response.context["top_threads"]), 1)
-        self.assertEqual(len(response.context["most_active_threads"]), 1)
-        self.assertEqual(len(response.context["pop_threads"]), 0)
+        response = self.client.get(reverse(
+            'hk_list_overview_top_threads', args=["list@example.com"]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context["threads"]), 1)
+        response = self.client.get(reverse(
+            'hk_list_overview_recent_threads', args=["list@example.com"]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context["threads"]), 1)
+        response = self.client.get(reverse(
+            'hk_list_overview_pop_threads', args=["list@example.com"]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context["threads"]), 0)
 
     def test_email_escaped_sender(self):
-        url = reverse('hk_list_overview', args=["list@example.com"])
-        response = self.client.get(url)
+        response = self.client.get(reverse(
+            'hk_list_overview_top_posters', args=["list@example.com"]))
+        self.assertContains(response, "dummy", status_code=200)
         self.assertNotContains(response, "dummy@example.com", status_code=200)
 
 
@@ -263,7 +294,11 @@ class PrivateArchivesTestCase(TestCase):
         # session.save()
         response = self.client.get(url, query)
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Dummy message")
+        try:
+            self.assertContains(response, "Dummy message")
+        except AssertionError:
+            print(response.content)
+            raise
 
     def test_month_view(self):
         now = datetime.datetime.now()
@@ -271,8 +306,13 @@ class PrivateArchivesTestCase(TestCase):
             'hk_archives_with_month',
             args=["list@example.com", now.year, now.month]))
 
-    def test_overview(self):
-        self._do_test(reverse('hk_list_overview', args=["list@example.com"]))
+    def test_overview_top_threads(self):
+        self._do_test(reverse(
+            'hk_list_overview_top_threads', args=["list@example.com"]))
+
+    def test_overview_recent_threads(self):
+        self._do_test(reverse(
+            'hk_list_overview_recent_threads', args=["list@example.com"]))
 
     def test_thread_view(self):
         self._do_test(reverse(

hyperkitty/urls.py
@@ -47,6 +47,18 @@ list_patterns = [
     url(r'^$', mlist.overview, name='hk_list_overview'),
     url(r'^recent-activity$',
         mlist.recent_activity, name='hk_list_recent_activity'),
+    url(r'^recent-threads$',
+        mlist.overview_recent_threads, name='hk_list_overview_recent_threads'),
+    url(r'^pop-threads$',
+        mlist.overview_pop_threads, name='hk_list_overview_pop_threads'),
+    url(r'^top-threads$',
+        mlist.overview_top_threads, name='hk_list_overview_top_threads'),
+    url(r'^favorites$',
+        mlist.overview_favorites, name='hk_list_overview_favorites'),
+    url(r'^posted-to$',
+        mlist.overview_posted_to, name='hk_list_overview_posted_to'),
+    url(r'^top-posters$',
+        mlist.overview_top_posters, name='hk_list_overview_top_posters'),
     url(r'^export/(?P<filename>[^/]+)\.mbox.gz$',
         mlist.export_mbox, name='hk_list_export_mbox'),
 ]

commit 2034b937c438f66b465d86bebbbc1209a197a1f1

Restore a JS event broken by a55d6493
Fixes #134

hyperkitty/static/hyperkitty/js/hyperkitty-overview.js
@@ -65,7 +65,7 @@ function setup_overview(recent_activity_url) {
     $('#posted-to h3').click(collapsibleDivs);
 
     // "More threads" links
-    $('.more-threads').click(function(e) {
+    $(".overview").on("click", ".more-threads", function(e) {
         e.preventDefault();
         //var more_block = $(this).parent('.more-threads');
         var more_block = $(this).parent().next();