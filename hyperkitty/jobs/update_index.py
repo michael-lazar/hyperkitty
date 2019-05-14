a0e66870f911c06e8bad6d6a7a4631f129c2ce01
Mark Sapiro <mark@msapiro.net>
2019-05-14 16:28:23 +0000

    Remove deprecation warning from update_index job.

--- hyperkitty/jobs/update_index.py
+++ hyperkitty/jobs/update_index.py
@@ -23,8 +23,6 @@
 Update the full-text index
 """
 
-import warnings
-
 from django_extensions.management.jobs import BaseJob
 
 from hyperkitty.lib.utils import run_with_lock
@@ -36,9 +34,4 @@ class Job(BaseJob):
     when = "hourly"
 
     def execute(self):
-
-        warnings.warn(
-            'update_index job has been deprecated and will be removed in '
-            'future releases. Use "python manage.py update_index --age 1"'
-            ' instead as an hourly cron job to update index every hour.')
         run_with_lock(update_index, remove=False)
